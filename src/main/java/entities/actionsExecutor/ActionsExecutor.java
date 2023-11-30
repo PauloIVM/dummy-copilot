0f1cac21af96db97e046cd285de75904255c0db8
Paulo Marinho <pakas_vieira_95@hotmail.com>
2023-11-30 10:36:59 -0300

    chore: add to-do comment

--- src/main/java/entities/actionsExecutor/ActionsExecutor.java
+++ src/main/java/entities/actionsExecutor/ActionsExecutor.java
@@ -5,6 +5,13 @@ import entities.action.ActionPaste;
 import entities.action.ActionSequence;
 import entities.action.ActionType;
 
+// TODO: Isso é uma entidade que depende de um IRobot. Em tese é até ok por termos
+// invertido a dependência. Contudo, o ideal da entidade eu acho que seria que ela
+// nem mesmo esperasse a 'ideia' de uma inerface. Uma maneira q eu poderia corrigir
+// isso seria utilizar um pattern similar ao Observer... tipo... a entidade faz um
+// subscribe de quem ela deve avisar quando uma action for disparada... acho que
+// seria um refactor interessante talvez, se não complicar demais.
+
 public class ActionsExecutor {
     private final IRobot robot;
 
